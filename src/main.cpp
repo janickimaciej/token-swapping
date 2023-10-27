85adc31ae280938cb7f7172949f9d42179e0c061
Adioos5 <adrian.ciesla.stud@pw.edu.pl>
2023-10-27 19:13:28 +0200

    Write basic code but don't test yet

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "token_swapping/bubble_sort.hpp"
+#include "token_swapping/special_sort.hpp"
 #include "token_swapping/instance.hpp"
 
 #include <iostream>
@@ -7,11 +8,12 @@
 
 int main()
 {
-	TokenSwapping::Instance instance(1, std::vector<int>{0, 4, 2, 3, 1});
-	std::vector<std::pair<int, int>> solution = TokenSwapping::BubbleSort::sort(instance);
+	TokenSwapping::Instance instance(2, std::vector<int>{0, 4, 2, 3, 1});
+	std::vector<std::pair<int, int>> solution = TokenSwapping::SpecialSort::sort(instance); // 4, 3, 2, 1, 0
 	for (const std::pair<int, int>& edge : solution)
 	{
 		std::cout << edge.first << " <-> " << edge.second << std::endl;
 	}
+
 	return 0;
 }
