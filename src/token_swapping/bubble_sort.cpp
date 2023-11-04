95e61cff31fe8d80a1445cd50d96cfcea0f0ee2c
janickimaciej <maciej.m.janicki@gmail.com>
2023-11-04 22:10:11 +0100

    Refactor code

--- src/token_swapping/bubble_sort.cpp
+++ src/token_swapping/bubble_sort.cpp
@@ -5,7 +5,7 @@
 
 namespace TokenSwapping
 {
-	std::vector<std::pair<int, int>> BubbleSort::sort(Instance instance)
+	std::vector<std::pair<int, int>> BubbleSort::getSolution(Instance instance)
 	{
 		std::vector<std::pair<int, int>> solution;
 		for (int i = 1; i < instance.size(); ++i)
